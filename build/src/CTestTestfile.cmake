75bbb039eb82d50e6b25dca07c44abeeeca46143
LauraHovhannisyan <121853374+LauraHovhannisyan@users.noreply.github.com>
2023-02-10 01:32:42 +0400

    Delete build directory

--- build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/ACA/ACA_BigInt/src
-# Build directory: C:/Users/User/ACA/ACA_BigInt/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("BigInt")
