08707ee7f26b38a643ddfaf97d6165e99c7e6ccc
LauraHovhannisyan <hovhannisyan.laira03@gmail.com>
2023-02-12 23:54:21 +0400

    changes in main

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ using namespace ACA;
 
 int main()
 {
-	BigInt num1="22507";
-    BigInt num2="234512003";
-    BigInt num3="0";
+	BigInt num1("22507");
+    BigInt num2("234512003");
+    BigInt num3=("0");
     BigInt num4 = std::string("1") + num3;
 
     std::cout<<(num1>num2)<<std::endl;
@@ -28,7 +28,7 @@ int main()
     //std::cin>>n;
     //std::cout<<n<<std::endl;
 
-    BigInt x="2",y="24";
+    BigInt x("2"),y("24");
     num3=x^y;
     return 0;
 }
